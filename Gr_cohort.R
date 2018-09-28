cf958d96d8148eeccb00833ec5e5536e93967a3b
Xin Wang <wangxin8923@gmail.com>
2018-09-28 12:35:00 -0400

    initialization of the algorithm

--- Gr_cohort.R
+++ Gr_cohort.R
@@ -8,6 +8,7 @@
 # lam: tuning parameter
 # maxiter: maximum number of iterations
 # tolabs and tolrel are two tolerance criteria in ADMM
+# model: model can be year or age. If year is specified, clustering is for year
 
 Gr_cohort <- function(year, age, y, x, betam0, model = "year",
                       lam = 0.5, nu = 1, gam = 3, lam = 0.5,
@@ -21,13 +22,20 @@ Gr_cohort <- function(year, age, y, x, betam0, model = "year",
   
   nyear <- length(uniq_year)
   nage <- length(uniq_age)
+  ncoh <- nyear + nage - 1 # number of cohort effects
   
-  nobs <- length(uniqxy)
-  npair <- nobs*(nobs-1)/2
+  cohort <- as.integer(factor(year - age))
   
   Ip <- diag(1,ncx,ncx)
   
-  D <- matrix(0,nobs*(nobs-1)/2,nobs)
+  if(model == "year")
+  {
+    nobs <- nyear
+    indexy <- year
+    uniq_index <- uniq_year
+  }
+  
+  D <- matrix(0,nobs*(nobs-1)/2,nobs)  # ei - ej
   for(j in 1:(nobs-1))
   {
     indexj <- (nobs-1 + nobs-j+1)*(j-1)/2
@@ -37,4 +45,67 @@ Gr_cohort <- function(year, age, y, x, betam0, model = "year",
   }
   
   AtA <- t(D)%*%D %x% Ip
+  
+  
+  Zc <- matrix(0, n0, ncoh) # cohort matrix
+  Zc[cbind(1:n0, cohort)] <- 1
+  
+  Hm <- matrix(0, 3, ncoh) # constraints matrix
+  Hm[1,] <- rep(1,ncoh)
+  Hm[2,]<-  1:ncoh
+  Hm[3,] <- (1:ncoh)^2
+  
+  
+  
+  #### transformation 
+  Xm <- matrix(0, n0, nobs*ncx)
+  Zcm <- Zc
+  ym <- rep(0, n0)
+  nJ <- rep(0, nobs)
+  for(i in 1:nobs)
+  {
+    indexi <- indexy == uniq_index[i]
+    ni <- sum(indexy == uniq_index[i])
+    nJ[i] <- ni
+    Xm[indexi, (ncx*(i-1) + 1) : (ncx*i)] <- x[indexy == uniq_index[i],]/sqrt(ni)
+    ym[indexi] <- y[indexi]/sqrt(ni)
+    Zcm[index,] <- Zc[index,]/sqrt(ni)
+  }
+  
+  
+  XtX_inv <- solve(t(Xm) %*% Xm + nu * AtA)
+  ZtZ_inv <- solve(t(Zcm) %*% Zcm + nu * t(Hm) %*% Hm)
+  Xty <- t(Xm) %*%ym
+  Zty <- t(Zcm) %*%ym
+  XtZ <- t(Xm) %*% Zcm
+  ZtX <- t(XtZ)
+  
+  reg_b1 <- XtX_inv %*% Xty
+  reg_eta1 <- ZtZ_inv %*% Zty
+  ##### some initials ####
+  deltam.old <- t(D %*% betam0)
+  betam <- betam0
+  betanew <- c(t(betam))
+  eta <- rep(0,ncoh)
+  
+  vm  <-  matrix(0, ncx, nobs*(nobs-1)/2)
+  vh <- rep(0, 3)
+  
+  flag <- 0
+  
+  for(m in 1:maxiter)
+  {
+    betanew <- reg_b1 - XtX_inv %*% XtZ %*% eta + nu* XtX_inv %*% c((deltam.old -  vm/nu) %*% D)
+    etanew <- reg_eta1 - ZtZ_inv %*% ZtX %*% betanew - t(Hm) %*% vh
+    
+    betadiff <- t(D %*% betam)
+    psim <- betadiff + vm/nu
+    deltam <- sapply(1:ncol(psim),function(xx) scad(psim[,xx],cvec[xx]*lam,nu,gam))
+    vm <- vm + nu * (betadiff - deltam)
+    vh <- vh + nu * Hm %*% etanew 
+  }
+  
+  
+  
+  
 }
